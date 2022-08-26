f22bf6f73881b830daf0a48d167ce77e9bad2303
wajahatabbas27 <wajahatabbas27@gmail.com>
2022-08-26 22:55:44 +0500

    upadte readme file

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCf0u7UMJamiPiJbdJcSgOMmDh1uEDVvaA",
+  authDomain: "todo-app-firebase-react-70d48.firebaseapp.com",
+  projectId: "todo-app-firebase-react-70d48",
+  storageBucket: "todo-app-firebase-react-70d48.appspot.com",
+  messagingSenderId: "72657263080",
+  appId: "1:72657263080:web:4e17790a5fab1fa9d7ac5d",
+  measurementId: "G-CFZ4ZT3YZW",
+};
